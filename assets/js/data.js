72be4a238e993f16732d5d218107917786e0f5a1
Elgun Ceferov <elgunceferov06@gmail.com>
2023-12-16 15:09:16 +0400

    finish data

--- assets/js/data.js
+++ assets/js/data.js
@@ -10,6 +10,11 @@ import movie5 from  '../images/movie-5.png'
 import movie6 from  '../images/movie-6.png'
 import movie7 from  '../images/movie-7.png'
 import movie8 from  '../images/movie-8.png'
+import series1 from  '../images/series-1.png'
+import series2 from  '../images/series-2.png'
+import series3 from  '../images/series-3.png'
+import series4 from  '../images/series-4.png'
+
 
 const moviedata = [
     {
@@ -74,7 +79,7 @@ const moviedata = [
         views:20000,
         likes:12000,
         image:{upcoming4},
-        iframe:"https://www.youtube.com/embed/ye63hQLDj4k?si=W-j6XwFVXTzvBscE"
+        iframe:"https://www.youtube.com/embed/x2YHPZMj8r4?si=JmbDyOyB51kO21eD"
     },
     {
         id: 5,
@@ -139,6 +144,70 @@ const moviedata = [
         likes:310000,
         image:{movie4},
         iframe:"https://www.youtube.com/embed/X2m-08cOAbc?si=kPJRF8-98pskvdPB"
+    },
+    {
+        id: 9,
+        name: "The Batman",
+        imdb: 7.9,
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora impedit placeat totam in iste molestias voluptates, dolore quisquam enim non consectetur blanditiis laboriosam architecto ullam ipsum voluptas accusamus reprehenderit dolores.",
+        releasedate: "2022",
+        time: "176 min",
+        category: [
+            "action", "crime","drama"
+        ],
+        resolution:"4k",
+        views:600000,
+        likes:500000,
+        image:{movie5},
+        iframe:"https://www.youtube.com/embed/mqqft2x_Aa4?si=ja-R0_Q2_abm1QpI"
+    },
+    {
+        id: 10,
+        name: "Uncharted",
+        imdb: 7.0,
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora impedit placeat totam in iste molestias voluptates, dolore quisquam enim non consectetur blanditiis laboriosam architecto ullam ipsum voluptas accusamus reprehenderit dolores.",
+        releasedate: "2022",
+        time: "116 min",
+        category: [
+            "action", "adventure",
+        ],
+        resolution:"hd",
+        views:400000,
+        likes:320000,
+        image:{movie6},
+        iframe:"https://www.youtube.com/embed/eHp3MbsCbMg?si=Dk9OMYiuctGabWnQ"
+    },
+    {
+        id: 11,
+        name: "Death On The Nile",
+        imdb: 6.5,
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora impedit placeat totam in iste molestias voluptates, dolore quisquam enim non consectetur blanditiis laboriosam architecto ullam ipsum voluptas accusamus reprehenderit dolores.",
+        releasedate: "2022",
+        time: "127 min",
+        category: [
+            "crime", "drama","mystery"
+        ],
+        resolution:"hd",
+        views:400000,
+        likes:320000,
+        image:{movie7},
+        iframe:"https://www.youtube.com/embed/dZRqB0JLizw?si=YpMCZhpl4bzh3ywv"
+    },
+    {
+        id: 12,
+        name: "The King's Man",
+        imdb: 7.0,
+        description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora impedit placeat totam in iste molestias voluptates, dolore quisquam enim non consectetur blanditiis laboriosam architecto ullam ipsum voluptas accusamus reprehenderit dolores.",
+        releasedate: "2022",
+        time: "131 min",
+        category: [
+            "action", "adventure","thriller"
+        ],
+        resolution:"hd",
+        views:123000,
+        likes:80000,
+        image:{movie8},
+        iframe:"https://www.youtube.com/embed/5zdBG-iGfes?si=iDPVCujIJSn07Czb"
     },
    
     
